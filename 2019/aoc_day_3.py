65c08f84c16f3eba5fb883cbbb3add295d7b5895
pkepley <pakepley@gmail.com>
2019-12-03 21:20:50 -0600

    Kind of ugly solutions to parts 1 and 2.

--- 2019/aoc_day_3.py
+++ 2019/aoc_day_3.py
@@ -29,20 +29,11 @@ def path_vertices(path_description):
 
     return vertices
 
-
 def path_edges(path_description):
     vertices = path_vertices(path_description)    
     edges = [[vertices[i], vertices[i+1]] for i in range(len(vertices) - 1)]
 
-    return edges
-
-
-def path_intersections(edges_A, edges_B):
-    edges_A = sorted([sorted(e) for e in edges_A])
-    edges_B = sorted([sorted(e) for e in edges_B])
-
-    pass
-
+    return edges    
 
 def segment_intersection(seg_A, seg_B):
     # Order the segments in dictionary order to avoid lots of comparisons below
@@ -78,7 +69,61 @@ def segment_intersection(seg_A, seg_B):
             return []
     else:
         return segment_intersection(seg_B, seg_A)
-        
+
+def path_intersections(edges_A, edges_B):
+    intersection_list = []
+    
+    for seg_A in edges_A:
+        for seg_B in edges_B:
+            intersection = segment_intersection(seg_A, seg_B)
+            if intersection:
+                intersection_list.extend(intersection)
+                
+    return intersection_list
+
+def edge_length(e):
+    return abs(e[1][0] - e[0][0]) + abs(e[1][1] - e[0][1])
+
+def path_nontrivial_intersection_steps(edges_A, edges_B, origin):
+    intersection_list = []
+    intersection_steps_list_A = []
+    intersection_steps_list_B = []    
+
+    steps_A = 0
+    steps_B = 0
+    
+    for seg_A in edges_A:
+        steps_B = 0
+        for seg_B in edges_B:
+            intersection = segment_intersection(seg_A, seg_B)
+            if intersection and intersection != [origin]:
+                intersection_list.extend(intersection)
+                intersection_steps_list_A.extend([steps_A + edge_length([seg_A[0], i]) for i in intersection])
+                intersection_steps_list_B.extend([steps_B + edge_length([seg_B[0], i]) for i in intersection])
+
+            steps_B += edge_length(seg_B)
+        steps_A += edge_length(seg_A)
+
+    intersection_total_steps = [a+b for (a,b) in zip(intersection_steps_list_A, intersection_steps_list_B)]
+
+    return intersection_total_steps
+
+def distance_to_nontrivial_intersection(origin, points):    
+    points = [p for p in points if p != origin]
+    distances = [abs(p[0] - origin[0]) + abs(p[1] - origin[1]) for p in points]
+
+    return min(distances)
+
+def solve_part_1(seg_descr_A, seg_descr_B):
+    intersections = path_intersections(path_edges(seg_descr_A), path_edges(seg_descr_B))
+    
+    return distance_to_nontrivial_intersection((0,0), intersections)
+
+def solve_part_2(seg_descr_A, seg_descr_B):
+    intersection_total_steps = path_nontrivial_intersection_steps(path_edges(seg_descr_A), path_edges(seg_descr_B), (0,0))
+
+    return min(intersection_total_steps)
+
 
 if __name__ == '__main__':
     # Test Vertically...
@@ -98,8 +143,24 @@ if __name__ == '__main__':
     assert(segment_intersection([(-5, 4), (5, 4)], [(0,0), (0, 10)]) == [(0, 4)])    
     assert(segment_intersection([(-5, 4), (-2, 4)], [(0,0), (0, 10)]) == [])    
 
+    # Test distance example 1:
+    seg_descr_A = ['R75','D30','R83','U83','L12','D49','R71','U7','L72']
+    seg_descr_B = ['U62','R66','U55','R34','D71','R55','D58','R83']
+    assert(solve_part_1(seg_descr_A, seg_descr_B) == 159)
+    assert(solve_part_2(seg_descr_A, seg_descr_B) == 610)
+    
+    # Test distance example 2:
+    seg_descr_A = ['R98','U47','R26','D63','R33','U87','L62','D20','R33','U53','R51']
+    seg_descr_B = ['U98','R91','D20','R16','D67','R40','U7','R15','U6','R7']
+    assert(solve_part_1(seg_descr_A, seg_descr_B) == 135)
+    assert(solve_part_2(seg_descr_A, seg_descr_B) == 410)    
 
-    # Solve things...    
-    print(get_input('./input/input_day_3.txt'))
-    print(path_vertices(['R75','D30','R83','U83','L12','D49','R71','U7','L72']))
-    print(path_edges(['R75','D30','R83','U83','L12','D49','R71','U7','L72']))
+    # Solve puzzle:    
+    seg_descrs = get_input('./input/input_day_3.txt')
+    seg_descr_A = seg_descrs[0]
+    seg_descr_B = seg_descrs[1]
+    s1 = solve_part_1(seg_descr_A, seg_descr_B)
+    s2 = solve_part_2(seg_descr_A, seg_descr_B)    
+    print("Solution to part 1: {0}".format(s1))
+    print("Solution to part 2: {0}".format(s2))    
+    
