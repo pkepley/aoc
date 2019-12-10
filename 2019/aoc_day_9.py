0fe9fe01c37b2eb6504bcdad6b53300e0b6b8de7
Paul Kepley <pakepley@gmail.com>
2019-12-09 22:36:46 -0600

    Day 9 Part 1.

---
+++ 2019/aoc_day_9.py
@@ -0,0 +1,16 @@
+from intcode_computer import intcode_computer
+
+def get_pgm_input(file_location):
+    f = open(file_location)
+    contents = [int(i) for i in ''.join(list(f)).split(',')]
+    f.close()
+    return contents
+
+if __name__ == '__main__':
+    pgm = get_pgm_input('./input/input_day_9.txt')
+
+    ic = intcode_computer(pgm)
+    ic.run([1])
+    s1 = ic.outputs[0]
+    print("Solution to Part 1: {}".format(s1))
+    
