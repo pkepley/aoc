d9d27ceb296ee630006b74f2e907fc9b3fcb62eb
Paul Kepley <pakepley@gmail.com>
2019-12-17 07:49:48 -0600

    Day 14: Ugly solution to part 2, need to fix.

--- 2019/aoc_day_14.py
+++ 2019/aoc_day_14.py
@@ -1,4 +1,4 @@
-from math import ceil
+from math import ceil, inf
 
 def get_input(file_path):
     f = open(file_path, 'r')
@@ -45,6 +45,11 @@ class chemical_factory:
                 'production' : 0,
                 'inventory'  : 0
             }
+
+    def clear(self):
+        for chem_name in self.chemicals.keys():
+            self.chemicals[chem_name]['production'] = 0
+            self.chemicals[chem_name]['inventory'] = 0            
                      
     def produce(self, chem_name, quantity):
         chem = self.chemicals[chem_name]
@@ -66,7 +71,43 @@ class chemical_factory:
             
         chemical['inventory'] -= quantity
                 
-            
+
+def find_max_production(rxns, available_ore):
+    ore_required = -1
+    
+    fuel_production = 1
+    lower_bound = 1
+    upper_bound = inf
+    cf = chemical_factory(rxns)
+    
+    while fuel_production < upper_bound:
+        fuel_production_multiplier = 1
+        
+        while ore_required <= available_ore:
+            fuel_production_multiplier *= 2
+            fuel_production = lower_bound + fuel_production_multiplier
+
+            # Clear factory prdouction and produce FUEL
+            cf.clear()
+            cf.produce('FUEL', fuel_production)        
+            ore_required = cf.chemicals['ORE']['production']
+
+        # Reset bounds
+        lower_bound = lower_bound + int(fuel_production_multiplier / 2)
+        upper_bound = lower_bound + fuel_production_multiplier
+        fuel_production = lower_bound
+
+        cf.clear()
+        cf.produce('FUEL', lower_bound)       
+        ore_required = cf.chemicals['ORE']['production']
+        if ore_required > available_ore:
+            break
+
+    # Back-off because we went one too far in tests
+    lower_bound -= 1
+
+    return lower_bound
+        
 if __name__ == '__main__':
     rxns = get_input('./input/input_day_14.txt')
 
@@ -75,3 +116,43 @@ if __name__ == '__main__':
     cf.produce('FUEL', 1)
     s1 = cf.chemicals['ORE']['production']
     print("Solution part 1: {}".format(s1))
+
+    # Solution to part 1
+    cf  = chemical_factory(rxns)
+    cf.produce('FUEL', 1)
+    s2 = find_max_production(rxns, 1000000000000)
+    print("Solution part 2: {}".format(s2))
+    
+    # rxn_str = '''157 ORE => 5 NZVS
+    # 165 ORE => 6 DCFZ
+    # 44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL
+    # 12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ
+    # 179 ORE => 7 PSHF
+    # 177 ORE => 5 HKGWZ
+    # 7 DCFZ, 7 PSHF => 2 XJWVT
+    # 165 ORE => 2 GPVTF
+    # 3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT'''
+    # rxns = parse_rxn_description(rxn_str)    
+    # print(find_max_production(rxns, 1000000000000))
+
+    # rxn_str = '''171 ORE => 8 CNZTR
+    # 7 ZLQW, 3 BMBT, 9 XCVML, 26 XMNCP, 1 WPTQ, 2 MZWV, 1 RJRHP => 4 PLWSL
+    # 114 ORE => 4 BHXH
+    # 14 VRPVC => 6 BMBT
+    # 6 BHXH, 18 KTJDG, 12 WPTQ, 7 PLWSL, 31 FHTLT, 37 ZDVW => 1 FUEL
+    # 6 WPTQ, 2 BMBT, 8 ZLQW, 18 KTJDG, 1 XMNCP, 6 MZWV, 1 RJRHP => 6 FHTLT
+    # 15 XDBXC, 2 LTCX, 1 VRPVC => 6 ZLQW
+    # 13 WPTQ, 10 LTCX, 3 RJRHP, 14 XMNCP, 2 MZWV, 1 ZLQW => 1 ZDVW
+    # 5 BMBT => 4 WPTQ
+    # 189 ORE => 9 KTJDG
+    # 1 MZWV, 17 XDBXC, 3 XCVML => 2 XMNCP
+    # 12 VRPVC, 27 CNZTR => 2 XDBXC
+    # 15 KTJDG, 12 BHXH => 5 XCVML
+    # 3 BHXH, 2 VRPVC => 7 MZWV
+    # 121 ORE => 7 VRPVC
+    # 7 XCVML => 6 RJRHP
+    # 5 BHXH, 4 VRPVC => 5 LTCX'''
+    # rxns = parse_rxn_description(rxn_str)    
+    # print(find_max_production(rxns, 1000000000000))
+
+    
