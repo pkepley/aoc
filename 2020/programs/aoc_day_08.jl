b6ff87054254c9dde29a2369c5d8c97eb8149e8e
Paul Kepley <pakepley@gmail.com>
2020-12-09 20:40:02 -0600

    A solution for 2020 Day 8 Part 1. Will re-work

---
+++ 2020/programs/aoc_day_08.jl
@@ -0,0 +1,114 @@
+### A Pluto.jl notebook ###
+# v0.12.16
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 0b6188b0-3a8b-11eb-112c-77145e1d99b5
+#= 
+🎅 Solutions for Day 8! 🎅
+
+🌟 Part 1: Fix a broken video game console! 🎮
+
+Intcode only comes once a year!
+=#
+
+# ╔═╡ 81e88cb0-3a8b-11eb-3aba-a3509bff5912
+function parse_input(s)
+	return split(s, "\n", keepempty=false)	
+end
+
+# ╔═╡ 2b544e8c-3a8d-11eb-3934-49cb1f5488a6
+function parse_op(op_str)
+	operation = split(op_str, ' ', keepempty = false)	
+	println(operation)
+	return (operation[1], parse(Int, operation[2]))
+end
+
+# ╔═╡ b3dae776-3a8c-11eb-1d9d-af404055e57d
+function parse_program(pgm)
+	instructions = parse_input(pgm)
+	instructions = [parse_op(op_str) for op_str ∈ instructions]
+	
+	return instructions
+end
+
+# ╔═╡ 1740ecec-3a8e-11eb-2b90-b99dc792e5a2
+function execute_until_reloop(pgm)
+	instructions = parse_program(pgm)
+	n_instructions = length(instructions)
+	
+	# execute until we see an index used twice
+	operation_executed = zeros(Bool, n_instructions)
+	idx, acc = 1, 0	
+	
+	while operation_executed[idx] == false
+		operation_executed[idx] = true
+		operation, argument = instructions[idx]
+		
+		if operation == "acc"
+			acc += argument
+			idx += 1
+			
+		elseif operation == "jmp"
+			idx += argument
+			
+		elseif operation == "nop"
+			idx += 1
+		end
+	end
+	
+	return acc
+	
+end
+
+# ╔═╡ f1109a84-3a8f-11eb-3ca8-2fdbdd1cc29a
+function solve_prob_1(pgm)
+	return execute_until_reloop(pgm)
+end
+
+# ╔═╡ 8e4e5560-3a8c-11eb-2612-577833ceee7e
+begin 
+	example_input="""nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6
+"""
+end
+
+# ╔═╡ f3f072bc-3a8d-11eb-3b54-719f2924e9d2
+@assert solve_prob_1(example_input) == 5
+
+# ╔═╡ ed7ca23c-3a8f-11eb-34fe-dd7cf712377b
+############################ Day 8 Solution #################################
+begin
+	input_path = "../input/input_day_08.txt"	
+	
+	problem_pgm = open(input_path) do file
+		read(file, String)
+	end	
+end
+
+# ╔═╡ 5b37cd42-3a90-11eb-25a3-6105e801b231
+part_1_soln = solve_prob_1(problem_pgm)
+
+# ╔═╡ 52110486-3a90-11eb-3a4f-11a08a8bd71f
+println("Day 8: Part 1: ", part_1_soln)
+
+# ╔═╡ Cell order:
+# ╠═0b6188b0-3a8b-11eb-112c-77145e1d99b5
+# ╠═81e88cb0-3a8b-11eb-3aba-a3509bff5912
+# ╠═2b544e8c-3a8d-11eb-3934-49cb1f5488a6
+# ╠═b3dae776-3a8c-11eb-1d9d-af404055e57d
+# ╠═1740ecec-3a8e-11eb-2b90-b99dc792e5a2
+# ╠═f1109a84-3a8f-11eb-3ca8-2fdbdd1cc29a
+# ╠═8e4e5560-3a8c-11eb-2612-577833ceee7e
+# ╠═f3f072bc-3a8d-11eb-3b54-719f2924e9d2
+# ╠═ed7ca23c-3a8f-11eb-34fe-dd7cf712377b
+# ╠═5b37cd42-3a90-11eb-25a3-6105e801b231
+# ╠═52110486-3a90-11eb-3a4f-11a08a8bd71f
