3342569290f4892f3be04cca22208d2ea755cdeb
Paul Kepley <pakepley@gmail.com>
2020-12-13 10:49:21 -0600

    Start point for a solution.

---
+++ 2020/programs/aoc_template.jl
@@ -0,0 +1,84 @@
+### A Pluto.jl notebook ###
+# v0.12.17
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 0b6188b0-3a8b-11eb-112c-77145e1d99b5
+#= 
+Today's Solutions! 
+
+🌟 Part 1: 
+=#
+
+# ╔═╡ b0bbb420-3d62-11eb-050a-99a665672dba
+DAY_NUMBER = "FILL_ME_IN!"
+
+# ╔═╡ 81e88cb0-3a8b-11eb-3aba-a3509bff5912
+function parse_input(s)
+	rows = split(s, "\n", keepempty=false)		
+	return rows
+end
+
+# ╔═╡ f1109a84-3a8f-11eb-3ca8-2fdbdd1cc29a
+function solve_prob_1()
+	return nothing
+end
+
+# ╔═╡ 129f488c-3a93-11eb-260e-8921353e1039
+function solve_prob_2(pgm)
+	return nothing
+end
+
+# ╔═╡ 8e4e5560-3a8c-11eb-2612-577833ceee7e
+############################ Examples+Tests ################################
+begin 
+	example_input="""
+???
+"""
+	example_pgm = parse_input(example_input)
+end
+
+# ╔═╡ f3f072bc-3a8d-11eb-3b54-719f2924e9d2
+begin
+	@assert solve_prob_1(example_pgm) == 0
+	@assert solve_prob_2(example_pgm) == 0
+end
+
+# ╔═╡ ed7ca23c-3a8f-11eb-34fe-dd7cf712377b
+############################ Solution ############################
+begin
+	input_path = "../input/input_day_$DAY_NUMBER.txt"
+	
+	problem_input_str = open(input_path) do file
+		read(file, String)
+	end	
+	
+	#problem_input = parse_program(problem_input)
+end
+
+# ╔═╡ 5b37cd42-3a90-11eb-25a3-6105e801b231
+part_1_soln = solve_prob_1(problem_input)
+
+# ╔═╡ 52110486-3a90-11eb-3a4f-11a08a8bd71f
+println("Day $DAY_NUMBER: Part 1: $part_1_soln")
+
+# ╔═╡ 3180ae1c-3a93-11eb-01ab-13cc9be93b67
+#part_2_soln = solve_prob_2(problem_input)
+
+# ╔═╡ 2c64697a-3a96-11eb-34ea-ff7f4a67aa99
+#println("Day $DAY_NUMBER: Part 2: $part_2_soln")
+
+# ╔═╡ Cell order:
+# ╠═0b6188b0-3a8b-11eb-112c-77145e1d99b5
+# ╠═b0bbb420-3d62-11eb-050a-99a665672dba
+# ╠═81e88cb0-3a8b-11eb-3aba-a3509bff5912
+# ╠═f1109a84-3a8f-11eb-3ca8-2fdbdd1cc29a
+# ╠═129f488c-3a93-11eb-260e-8921353e1039
+# ╠═8e4e5560-3a8c-11eb-2612-577833ceee7e
+# ╠═f3f072bc-3a8d-11eb-3b54-719f2924e9d2
+# ╠═ed7ca23c-3a8f-11eb-34fe-dd7cf712377b
+# ╠═5b37cd42-3a90-11eb-25a3-6105e801b231
+# ╠═52110486-3a90-11eb-3a4f-11a08a8bd71f
+# ╠═3180ae1c-3a93-11eb-01ab-13cc9be93b67
+# ╠═2c64697a-3a96-11eb-34ea-ff7f4a67aa99
