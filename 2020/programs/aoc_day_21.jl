128b8bd887114909a9c68c526475d18a21273b72
Paul Kepley <pakepley@gmail.com>
2020-12-24 23:26:05 -0600

    Fix indentation.

--- 2020/programs/aoc_day_21.jl
+++ 2020/programs/aoc_day_21.jl
@@ -8,7 +8,7 @@ using InteractiveUtils
 #= 
 🎅 Solutions for Day 21! 🎅
 
-🌟 Part 1: Count the non-allergenic ingredients! 🥛🐟🥜
+🌟 Part 1: Count the non-allergenic ingredients with repetition! 🥛🐟🥜
 🌟 Part 2: Get the list of allergenic ingredients! 🥛🐟🥜
 
 =#
@@ -19,95 +19,95 @@ DAY_NUMBER = 21
 # ╔═╡ 81e88cb0-3a8b-11eb-3aba-a3509bff5912
 function parse_input(s)
     rows = split(s, "\n", keepempty=false)	
-	
-	# parse the ingredients
-	ingredient_lists = map(r -> split(r, "(", keepempty=false)[1], rows)
-	ingredient_lists = map(r -> Set(split(r, " ", keepempty=false)), ingredient_lists)
-
-	# parse the allergens
-	allergen_lists = map(r -> split(r, "(", keepempty=false)[2], rows)
-	allergen_lists = map(r -> replace(r, "contains " => ""), allergen_lists)
-	allergen_lists = map(r -> replace(r, ")" => ""), allergen_lists)
-	allergen_lists = map(r -> Set(split(r, ", ", keepempty=false)), allergen_lists)	
-	
+    
+    # parse the ingredients
+    ingredient_lists = map(r -> split(r, "(", keepempty=false)[1], rows)
+    ingredient_lists = map(r -> Set(split(r, " ", keepempty=false)), ingredient_lists)
+    
+    # parse the allergens
+    allergen_lists = map(r -> split(r, "(", keepempty=false)[2], rows)
+    allergen_lists = map(r -> replace(r, "contains " => ""), allergen_lists)
+    allergen_lists = map(r -> replace(r, ")" => ""), allergen_lists)
+    allergen_lists = map(r -> Set(split(r, ", ", keepempty=false)), allergen_lists)	
+    
     return ingredient_lists, allergen_lists
 end
 
 # ╔═╡ 15db1808-454e-11eb-1378-4d323d57f2e0
 function determine_allergen_ingredients(ingredient_lists, allergen_lists)	
-	"""
+    """
 	Determine which ingredients have allergens, and return the mapping 
 	from ingredients to allergens.
 	"""
+    
+    # union all of the sets of allergens
+    unique_allergens = foldl(union, allergen_lists)
+    
+    # determine ingredient candidates
+    ingredient_cands = Dict()	
+    for allergen ∈ unique_allergens
+	idx_allergen = findall(e -> (allergen ∈ e), allergen_lists)	
 	
-	# union all of the sets of allergens
-	unique_allergens = foldl(union, allergen_lists)
-	
-	# determine ingredient candidates
-	ingredient_cands = Dict()	
-	for allergen ∈ unique_allergens
-		idx_allergen = findall(e -> (allergen ∈ e), allergen_lists)	
+	ingredient_cands[allergen] = 
+	    foldl(intersect, deepcopy(ingredient_lists[idx_allergen]))
+    end
+    
+    # mapping from allergenic-ingredients to allergens
+    ingredient_to_allergen = Dict()
+    
+    while length(ingredient_cands) > 0
+	for allergen ∈ keys(ingredient_cands)
+	    if length(ingredient_cands[allergen]) == 1				
+		# allergen only has one candidate ingredient, so is uniquely
+		# determined. add ingredient --> allergen pair to mapping.
+		ingredient = pop!(ingredient_cands[allergen])
+		ingredient_to_allergen[ingredient] = allergen
 		
-		ingredient_cands[allergen] = 
-		foldl(intersect, deepcopy(ingredient_lists[idx_allergen]))
-	end
-	
-	# mapping from allergenic-ingredients to allerge
-	ingredient_to_allergen = Dict()
-	
-	while length(ingredient_cands) > 0
-		for allergen ∈ keys(ingredient_cands)
-			if length(ingredient_cands[allergen]) == 1				
-				# allergen only has one candidate ingredient, so is uniquely
-				# determined. so add ingredient --> allergen pair to mapping
-				ingredient = pop!(ingredient_cands[allergen])
-				ingredient_to_allergen[ingredient] = allergen
-				
-				# remove allergen from the list of undetermined allergens
-				delete!(ingredient_cands, allergen)
-					
-				# delete ingredient from the candidates for all other allergens
-				for allergen2 ∈ keys(ingredient_cands) 
-					delete!(ingredient_cands[allergen2], ingredient)
-				end
-			end
-		end		
-	end
-	
-	return ingredient_to_allergen
+		# remove allergen from the list of undetermined allergens
+		delete!(ingredient_cands, allergen)
+		
+		# delete ingredient from the candidates for all other allergens
+		for allergen2 ∈ keys(ingredient_cands) 
+		    delete!(ingredient_cands[allergen2], ingredient)
+		end
+	    end
+	end		
+    end
+    
+    return ingredient_to_allergen
 end
 
 # ╔═╡ f1109a84-3a8f-11eb-3ca8-2fdbdd1cc29a
 function solve_prob_1(inpt)
     (ingredient_lists, allergen_lists) = parse_input(inpt)	
-	ingredient_to_allergen = determine_allergen_ingredients(ingredient_lists, 
-				                                            allergen_lists)	
-	
-	# list the allergenic ingredients
-	allergenic_ingredients = [k for k ∈ keys(ingredient_to_allergen)]
-	
-	# count the non-allergenic ingredients in the list
-	cnt_nonallergenic = 0
-	for ingredients ∈ ingredient_lists
-		cnt_nonallergenic += length([i for i ∈ ingredients if i ∉ allergenic_ingredients])
-	end
-	
+    ingredient_to_allergen = determine_allergen_ingredients(ingredient_lists, 
+				                            allergen_lists)	
+    
+    # list the allergenic ingredients
+    allergenic_ingredients = [k for k ∈ keys(ingredient_to_allergen)]
+    
+    # count the non-allergenic ingredients in the list
+    cnt_nonallergenic = 0
+    for ingredients ∈ ingredient_lists
+	cnt_nonallergenic += length([i for i ∈ ingredients if i ∉ allergenic_ingredients])
+    end
+    
     return cnt_nonallergenic
 end
 
 # ╔═╡ 129f488c-3a93-11eb-260e-8921353e1039
 function solve_prob_2(inpt)
     (ingredient_lists, allergen_lists) = parse_input(inpt)	
-	ingredient_to_allergen = determine_allergen_ingredients(ingredient_lists, 
-				                                            allergen_lists)	
-	
-	# sort the allergenic ingredients by their corresponding allergen
-	# ie sort the dict by the values, using this method:
-	#     https://stackoverflow.com/a/29904797/3677367
-	allergenic_ingredients = [k[1] for k ∈ sort(collect(ingredient_to_allergen),
-				by=x->x[2])]
-	
-	println(allergenic_ingredients)
+    ingredient_to_allergen = determine_allergen_ingredients(ingredient_lists, 
+				                            allergen_lists)	
+    
+    # sort the allergenic ingredients by their corresponding allergen
+    # ie sort the dict by the values, using this method:
+    #     https://stackoverflow.com/a/29904797/3677367
+    allergenic_ingredients = [k[1] for k ∈ sort(collect(ingredient_to_allergen),
+				                by=x->x[2])]
+
+    # return canonical dangerous ingredient list
     return join(allergenic_ingredients, ",")
 end
 
